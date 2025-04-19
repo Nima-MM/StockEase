05d01af435ca43e84af494a69dd3434dbfcc835b
Nima Mir Marashi <115414172+Nima-MM@users.noreply.github.com>
2025-04-19 14:50:16 +0200

    Update the branch responsible for primevue migration (#17)
    * migrate to primevue(#15)
    
    * uninstall vuetify dependencies, imports and calls
    
    - Remove unused dependencies from `package.json`
    - Clean up imports and function calls from source files (`Main.ts`)
    - Verify `vite.config.mts` is already clean; no changes required
    
    * install primevue dependencies, implement into main and create test component.
    
    * add tailwindcss 4.1 via vite...
    
    ...
    `npm install tailwindcss @tailwindcss/vite`
    implement into vite.config.mts
    and @import in style.css
    
    no scss/sass implementation yet
    
    * replace login-form, main-layout, login-layout, drawer and app-bar with primevue
    
    * add primeicons
    
    * add app-bar to top and update drawer
    
    * style login account in position, size and bg. add remember me 2 login.
    
    * add storage tabs for different storages. add product-table and data.
    
    * add search bar to product table for global keyword searching in THE table
    
    * create primevue.config.ts so there is global primevue components and initiations
    
    * create Unit-Test for product.component.ts initialization
    
    * move retrieve to store, so instead of provide/inject, the pinia store provides services
    
    * add 'blanc' buttons to product-table UI
    
    * ersetze bestehende vuetify komponenten mit custom new UI(#14)
    
    * add MVP decrease Stock
    
    * update style of decrease stock dialog. add and change theme to Nora
    
    * add MVP increase Stock
    
    * add MVP delete Entity to product-table. update style of navbar and drawer
    
    * add MVP edit to product-table
    
    * fix edit dialog by adjust service endpoint, no null models and allocation in lifecycle hook
    
    * add row expansion to product table. remove routing from entities table as it causes loading problems
    
    * fix in drawer the warehouse selector
    
    * add config stock-manager host
    
    * update main-nav-drawer with content
    
    * add sorting functionality to product table
    
    * feat(#6): add MVP create Product

---
+++ src/main/webapp/app/entities/product/product-dialogs/new-product-dialog.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="card flex justify-center">
+    <Button
+      variant="text"
+      type="button"
+      icon="pi pi-plus"
+      label="Neues Produkt"
+      class="p-button-outlined"
+      severity="info"
+      @click="visible = true"
+    />
+
+    <Dialog v-model:visible="visible" modal pt:root:class="!border-0 " pt:mask:class="backdrop-blur-sm">
+      <template #container>
+        <header class="p-4 bg-gradient-to-r from-blue-500 to-blue-700">
+          <span class="pi pi-pencil p-4"></span>
+          <span class="font-bold text-xl dark:text-primary-50">NEUES PRODUKT</span>
+        </header>
+
+        <!-- content -->
+        <div class="flex flex-col px-8 py-8 gap-6 rounded-2xl">
+          <FloatLabel variant="on">
+            <label for="on_ean" class="!text-primary-50 !z-1">EAN</label>
+            <InputText
+              v-model="product.ean"
+              id="on_ean"
+              class="focus:!border-[var(--p-blue-500)] !border-[var(--p-primary-50)] focus:!outline-none !border-1 !text-blue-400"
+              fluid
+            />
+          </FloatLabel>
+          <!-- name and stock -->
+          <div class="flex gap-2">
+            <div class="!w-4/6">
+              <FloatLabel variant="on" class="">
+                <label for="on_name" class="!text-primary-50 !z-1">Produktname</label>
+                <InputText
+                  v-model="product.name"
+                  id="on_name"
+                  class="!w-full focus:!border-[var(--p-blue-500)] !border-[var(--p-primary-50)] focus:!outline-none !border-1 !text-blue-400"
+                />
+              </FloatLabel>
+            </div>
+            <div class="!w-2/6">
+              <FloatLabel variant="on">
+                <label for="on_stock" class="!text-primary-50 !z-1">Bestand</label>
+                <InputNumber
+                  v-model="product.stock"
+                  inputId="on_stock"
+                  suffix=" Stück"
+                  inputClass="focus:!border-[var(--p-blue-500)] !border-[var(--p-primary-50)] focus:!outline-none !border-1 !text-blue-400"
+                  fluid
+                />
+              </FloatLabel>
+            </div>
+          </div>
+          <!-- TODO: outline color -->
+          <div class="flex gap-2">
+            <div class="!w-2/6">
+              <FloatLabel variant="on" class="">
+                <label for="on_brand" class="!text-primary-50 !z-1">Marke</label>
+                <Select
+                  v-model="product.brand.name"
+                  inputId="on_brand"
+                  :options="brandNames"
+                  @update:model-value="updateBrand"
+                  class="!w-full focus:!border-[var(--p-blue-500)] !border-[var(--p-primary-50)] focus:!outline-none !border-1 !text-blue-400"
+                />
+              </FloatLabel>
+            </div>
+            <div class="!w-2/6">
+              <FloatLabel variant="on" class="">
+                <label for="on_color" class="!text-primary-50 !z-1">Farbe</label>
+                <Select
+                  v-model="product.color.name"
+                  inputId="on_color"
+                  :options="colorNames"
+                  @update:model-value="updateColor"
+                  class="!w-full focus:!border-[var(--p-blue-500)] !border-[var(--p-primary-50)] focus:!outline-none !border-1 !text-blue-400"
+                />
+              </FloatLabel>
+            </div>
+            <div class="!w-2/6">
+              <FloatLabel variant="on" class="">
+                <label for="on_category" class="!text-primary-50 !z-1">Kategorie</label>
+                <Select
+                  v-model="product.category.name"
+                  inputId="on_category"
+                  :options="categoryNames"
+                  @update:model-value="updateCategory"
+                  class="!w-full focus:!border-[var(--p-blue-500)] !border-[var(--p-primary-50)] focus:!outline-none !border-1 !text-blue-400"
+                />
+              </FloatLabel>
+            </div>
+          </div>
+
+          <!-- action buttons -->
+          <div class="flex items-center gap-4">
+            <Button
+              label="Abbrechen"
+              @click="visible = false"
+              text
+              class="!p-4 w-full !text-surface-0 !border !border-white/50 hover:!bg-white/25"
+            ></Button>
+            <Button
+              label="Erstellen"
+              @click="confirmNewProduct"
+              text
+              class="!p-4 w-full !text-blue-400 !border !border-white/50 hover:!bg-white/25"
+            ></Button>
+          </div>
+        </div>
+      </template>
+    </Dialog>
+  </div>
+</template>
+<script lang="ts" src="./new-product-dialog.component"></script>
